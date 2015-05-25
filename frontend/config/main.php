c0c6c8d6a9edc716090d899abc6d959a3efbe42a
maxwelldu <dcj3sjt@163.com>
2015-05-25 12:39:54 +0800

    init yii2 framework

---
+++ frontend/config/main.php
@@ -0,0 +1,43 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'components' => [
+	'memcache' => [
+                'class' => '\yii\caching\MemCache',
+                'servers' => [
+                        [
+                                'host' => '127.0.0.1',
+                                'port' => 11211,
+                                'weight' => 100,
+                        ],
+                ],
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+    ],
+    'params' => $params,
+];
