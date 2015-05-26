c72ece2efe892bfab632e424c440cebf9ad15f38
maxwelldu <dcj3sjt@163.com>
2015-05-26 10:13:58 +0800

    change default route as redis

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
+    'defaultRoute' => 'redis',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
