6429a25b8bb0268f18b388646b671639e6ff0f3f
zhangteng <tenger35@163.com>
2015-05-26 13:27:24 +0800

    update login view font

--- frontend/views/redis/login.php
+++ frontend/views/redis/login.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <?= $form->field($model, 'email') ?>
             <?= $form->field($model, 'password')->passwordInput() ?>
             <div class="form-group">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                <?= Html::submitButton('登录', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
             </div>
             <?php ActiveForm::end(); ?>
         </div>
