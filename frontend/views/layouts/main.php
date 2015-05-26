61ca80510ef29dec2d9beff88183d5d595b878dd
maxwelldu <dcj3sjt@163.com>
2015-05-26 18:14:05 +0800

    change footer content

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -74,7 +74,7 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-        <p class="pull-left">&copy; use redis as database <?= date('Y') ?></p>
+        <p class="pull-left">&copy; <?= date('Y') ?></p>
         <p class="pull-right"><?= Yii::powered() ?></p>
         </div>
     </footer>
