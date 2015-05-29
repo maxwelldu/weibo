df53ac6d5ac0195e1b3b3a44393769c64dd04784
maxwelldu <dcj3sjt@163.com>
2015-05-29 16:47:38 +0800

    add sphinx componser json and config components

--- common/config/main.php
+++ common/config/main.php
@@ -2,7 +2,7 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        'cache' => [
+        'cache' => [//使用memcached作缓存
             'class' => '\yii\caching\MemCache',
             'useMemcached' => true,
             'servers' => [
@@ -13,10 +13,23 @@ return [
                 ],
             ],
         ],
-//        'session' => [
-//            'class' => '\yii\web\CacheSession',
-//            'cache' => 'cache',
-//        ],
+        'session' => [//配置session使用上面的缓存组件服务
+            'class' => '\yii\web\CacheSession',
+            'cache' => 'cache',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=rdsnnamnbnnamnbprivate.mysql.rds.aliyuncs.com;dbname=blog',
+            'username' => 'maxwelldu',
+            'password' => 'yu13jiu14',
+            'charset' => 'utf8',
+        ],
+        'sphinx' => [
+            'class' => 'yii\sphinx\Connection',
+            'dsn' => 'mysql:host=rdsnnamnbnnamnbprivate.mysql.rds.aliyuncs.com;dbname=blog',
+            'username' => 'maxwelldu',
+            'password' => 'yu13jiu14',
+        ],
     ],
     'language' => 'zh-CN',
 ];
